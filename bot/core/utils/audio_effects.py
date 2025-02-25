ff2c4cd49e92846dbbbc559ab62f0ee7e8af7e9c
꯭➺ 🍃⃟AR Rᴀʜᴀᴛ🜲 <premiumbin08@gmail.com>
2025-02-26 01:05:58 +0600

    Create audio_effects.py

---
+++ bot/core/utils/audio_effects.py
@@ -0,0 +1,13 @@
+from pydub import AudioSegment
+import noisereduce as nr
+
+class AudioEffects:
+    @staticmethod
+    def apply_equalizer(audio_path, eq_preset):
+        # Equalizer implementation
+        pass
+
+    @staticmethod
+    def remove_vocal(audio_path):
+        # Vocal removal logic
+        pass
