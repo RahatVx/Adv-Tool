5984c9b7a89cea9df43aa3042f69a54fa0361f4a
꯭➺ 🍃⃟AR Rᴀʜᴀᴛ🜲 <premiumbin08@gmail.com>
2025-02-26 01:06:42 +0600

    Create video_effects.py

---
+++ bot/core/utils/video_effects.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+from moviepy.editor import VideoFileClip
+
+class VideoEffects:
+    @staticmethod
+    def stabilize_video(input_path, output_path):
+        # Video stabilization logic
+        pass
+
+    @staticmethod
+    def add_watermark(video_path, watermark_path, output_path):
+        # Watermark addition logic
+        pass
